d90e03fad80695e4ad6bf355e60fcf0cc77b3a8f
James Wu <jwu@efunda.com>
2015-01-24 01:10:11 -0800

    formatted request string

--- src/client.hpp
+++ src/client.hpp
@@ -22,36 +22,76 @@
 #ifndef SBT_CLIENT_HPP
 #define SBT_CLIENT_HPP
 
+#define BUFFER_SIZE 4096
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <sstream>
+
 #include "common.hpp"
+#include "meta-info.hpp"
+#include "http/url-encoding.hpp"
+
+using namespace std;
 
 namespace sbt {
+
+enum eventTypes : int {
+  kStarted = 0,
+  kCompleted = 1,
+  kStopped = 2,
+};
 
 class Client
 {
 public:
   Client(const std::string& port, const std::string& torrent)
   {
-    // create socket using TCP IP
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons();
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
-
-    // connect to the server
-    if (connect(sockfd, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) == -1) {
-      perror("connect");
-      return 2;
-    }
-
-    sbt::MetaInfo nInfo = new MetaInfo();
-    istream torrentStream = std::istringstream is(torrent);
-    nInfo.wireDecode(torrentStream);
-
-    string requestLine = nInfo.getAnnounce(); 
+    nPort = port;
+    nInfo = new MetaInfo();
   }
+
+  string prepareRequest(int event) {
+    string url_f = "/announce?info_hash=%s&peer_id=%s&port=%s&uploaded=0&downloaded=0&left=%d";
+
+    const char* url_hash = (url::encode((const uint8_t *)(nInfo->getHash()).get(), 20)).c_str();
+    const char* url_peer_id = (url::encode((const uint8_t *)nPeerId.c_str(), 20)).c_str();
+
+    string url_event = "";
+    switch(event) {
+      case kStarted:
+        url_event = "&event=started";
+        break;
+      case kCompleted:
+        url_event = "&event=completed";
+        break;
+      case kStopped:
+        url_event = "&event=stopped";
+        break;
+    }
+    url_f += url_event;
+
+    const char* url_f_c = url_f.c_str();
+    char request_url[BUFFER_SIZE];
+    sprintf(request_url, url_f_c, url_hash, url_peer_id, nPort.c_str());
+    string request = request_url;
+
+    fprintf(stdout, "%s", request_url);
+    return request;
+  }
+
+private:
+  MetaInfo* nInfo;
+  string nPort;
+  string nPeerId;
 };
 
 } // namespace sbt
